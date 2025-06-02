cdd48e6878013d83bf73af974adcf58b4b1b6d56
LeonardoSTH <leonardoloteriodelima@gmail.com>
2025-06-02 14:53:35 -0300

    update database dump

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,4 +1,5 @@
-//const jest = require("jest");
+const request = require("supertest");
+const admin = require("../routes/admin");
 
 describe("Favoritar evento", () => {
   let evento = {
@@ -9,12 +10,27 @@ describe("Favoritar evento", () => {
   };
 
   it("Quando estiver null deve favoritar", () => {
-    favoritar(evento);
+    admin.favoritar(evento);
     expect(evento.favorito).toBe(true);
   });
 
   it("Quando já favorito deve remover o favorito", () => {
     favoritar(evento);
-    expect(evento.favorito).toBe(false);
+    admin.expect(evento.favorito).toBe(false);
+  });
+});
+
+describe("Post login", () => {
+  it("Enviando os dados de login do usuário deve retornar sucesso", async () => {
+    const res = await request(admin)
+      .post("/api/posts")
+      .send({
+        body: {
+          email: "leonardo@gmail.com",
+          senha: "123",
+        },
+      });
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toHaveProperty("post");
   });
 });
